8970b879be4481715cd14783f1cc3a8f4c7448f0
billy balekage <billybalekage@gmail.com>
2026-07-30 11:23:54 +0200

    correction de la syntase dans main.py pour fastapi

--- calculs/main.py
+++ calculs/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastApi 
+from fastapi import FastAPI
 
-app = FastApi()
+app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return ("message": "L'api de calcul et coordination des protections electrique")
+    resultat = {"message": "L'api de calcul et coordination des protections electrique"}
+    print("resulta de la root", resultat)
+    return resultat
